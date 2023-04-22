895c5c74507fb041ae44f2d0acc83b1f7f5d975d
indianahuey <indianahuey@gmail.com>
2023-04-22 16:51:40 -0700

    is really confused by absolute discounting...

--- code/dev.py
+++ code/dev.py
@@ -12,10 +12,11 @@ from math import log10
 
 class Trigram_LM_Model:
 
-    def __init__(self, train_filename, vocab_filename, absolute_discount, trigram_weight, bigram_weight):
-        self.absolute_discount = absolute_discount
+    def __init__(self, train_filename, vocab_filename, trigram_weight, bigram_weight, absolute_discount, katz_discount):
         self.trigram_weight = trigram_weight
         self.bigram_weight = bigram_weight
+        self.absolute_discount = absolute_discount
+        self.katz_discount = katz_discount
 
         with open(vocab_filename) as f:
             self.read_vocab(f)
@@ -32,6 +33,9 @@ class Trigram_LM_Model:
         self.get_counts(train_file)
 
         self.get_nr_counts()
+
+        self.abc_alphas = {}
+        self.bc_alphas = {}
 
         self.abx_total_counts = {}
         self.bx_total_counts = {}
@@ -129,17 +133,69 @@ class Trigram_LM_Model:
 
             smoothed_prob = r_star / N
 
-        elif smoothing_technique = 'linear interpolation':
-            mle_trigram_prob = self.trigram_counts.get(a, {}).get(b, {}).get(c, 0) / self.bigram_counts.get(a, {}).get(b, 1)
+        elif smoothing_technique == 'linear interpolation':
+            mle_trigram_prob = self.trigram_counts.get(a, {}).get(b, {}).get(c, 0) / self.bigram_counts.get(a, {}).get(b, 0)
             mle_bigram_prob = self.bigram_counts.get(b, {}).get(c, 0) / self.unigram_counts.get(b, 0)
             mle_unigram_prob = self.unigram_counts.get(c, 0)
 
             smoothed_prob = (
-                (trigram_weight * mle_trigram_prob) +
-                (bigram_weight * mle_bigram_prob) +
-                ((1 - (trigram_weight + bigram_weight)) * mle_unigram_prob)
-            ) 
+                (self.trigram_weight * mle_trigram_prob) +
+                (self.bigram_weight * mle_bigram_prob) +
+                ((1 - (self.trigram_weight + self.bigram_weight)) * mle_unigram_prob)
+            )
 
+        elif smoothing_technique == 'absolute discounting':
+            abc_count = self.trigram_counts.get(a, {}).get(b, {}).get(c, 0)
+            ab_count = self.bigram_counts.get(a, {}).get(b, 0)
+
+            bc_count = self.bigram_counts.get(b, {}).get(c, 0)
+            c_count = self.unigram_counts.get(c, 0)
+
+            if abc_count > 0:
+                smoothed_prob = (
+                    (abc_count - self.absolute_discount) /
+                    ab_count
+                ) if ab_count > 0 else 0
+
+            elif bc_count > 0:
+                if (a, b, c) in self.abc_alphas:
+                    abc_alpha = self.abc_alphas((a, b, c))
+                else:
+                    abc_alpha = self.compute_abc_alpha()
+                    self.abc_alphas((a, b, c)) = abc_alpha
+
+
+                discounted_bigram_prob = (
+                    (bc_count - self.absolute_discount) /
+                    c_count
+                ) if c_count > 0 else 0
+
+                smoothed_prob = abc_alpha * discounted_bigram_prob
+
+            else:
+                if (a, b c) in self.abc_alphas:
+                    abc_alpha = self.abc_alphas((a, b, c))
+                else:
+
+
+                if (b, c) in self.bc_alphas:
+                    bc_alpha = self.bc_alphas((b, c))
+                else:
+                    pass
+
+
+                mle_unigram_prob = (
+                    c_count /
+                    self.total_unigram_count
+                )
+
+                smoothed_prob = abc_alpha * bc_alpha * mle_unigram_prob
+
+
+
+        # TODO: What... does this implement Absolute or Kneser-Ney?
+        # TODO: I think this maybe implements un-modified Kneser-Ney
+        # TODO: replace var self.absolute_discount
         elif smoothing_technique == 'absolute discounting':
             # trigram-level terms
             abc_count = self.trigram_counts.get(a, {}).get(b, {}).get(c, 0)
@@ -199,6 +255,12 @@ class Trigram_LM_Model:
                 )
             )
 
+        elif smoothing_technique == 'katz':
+            pass
+
+        elif smoothing_technique == 'kneser-ney':
+            pass
+
         else:
             print('invalid smoothing technique')
             smoothed_prob = 0
@@ -213,6 +275,7 @@ def main():
     trigram_weight = 0.6
     bigram_weight = 0.3
     absolute_discount = 0.1
+    katz_discount = 0.2
 
     if trigram_weight + bigram_weight > 1:
         print('trigram weight + bigram weight must be less than or equal to 1')
@@ -222,10 +285,12 @@ def main():
         print('absolute discount must be in [0, 1]')
         return
 
-    model = Trigram_LM_Model(train_filename, vocab_filename, absolute_discount)
+    model = Trigram_LM_Model(train_filename, vocab_filename, trigram_weight, bigram_weight, absolute_discount, katz_discount)
 
     print(model.perplexity(test_filename, 'absolute discounting'))
     print(model.perplexity(test_filename, 'good-turing'))
+    print(model.perplexity(test_filename, 'linear interpolation'))
+
 
 
 if __name__ == '__main__':
